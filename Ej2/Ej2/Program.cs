960c57e124a9fa83ec0c595ae35bc5131e945053
Anthony Javier Santana Polanco <anthony26uu@gmail.com>
2017-01-12 23:15:33 -0400

    ModificacionEJ2

---
+++ Ej2/Ej2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anthony Santana Polanco "); // Escribe por pantalla un string 
+            Console.ReadKey()
+            // Falta punto y coma el copilador antes que el programa
+            //sea copilado me indica de la existencia del mismo. Tambien nos indica que se esperaba para evitar el error en este caso se esperaba un punto y coma (;)
+        }
+    }
+}
