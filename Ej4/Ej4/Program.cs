5292489c257e05d03849b488f04e170c49ecfb5c
Anthony Javier Santana Polanco <anthony26uu@gmail.com>
2017-01-12 23:21:10 -0400

    ModificacionEj4

---
+++ Ej4/Ej4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anthony Santana Polanco "); // Escribe por pantalla un string 
+            Console.ReadKey();
+            //Explorador de soluciones  ofrece informacion sobre el programa entre ella las clases que el mismo contiene, sus propiedades y preferencias entre otras cosas.
+        }
+    }
+}
