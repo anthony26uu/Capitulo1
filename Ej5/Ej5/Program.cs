26ad1aa7cc66e56b739a871ad3cf1bac3f47c7b0
Anthony Javier Santana Polanco <anthony26uu@gmail.com>
2017-01-12 23:22:31 -0400

    ModificacionEj5

---
+++ Ej5/Ej5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("M nombre es: Anthony Santana Polanco \n "); // Escribe por pantalla un string 
+            Console.WriteLine("Mi matricula es: 2014-0527");
+            Console.ReadKey();
+
+        }
+    }
+}
